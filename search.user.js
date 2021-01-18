c6efdf7afc78d27ea98cdeea7b487dc479328ac2
Gaurav Singh <75254747+gauravnumber@users.noreply.github.com>
2021-01-18 13:02:45 +0530

    added search userscript

---
+++ search.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        ddg enhancer
+// @namespace   Violentmonkey Scripts
+// @match       https://duckduckgo.com/
+// @match       https://www.google.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 11/13/2020, 10:04:35 PM
+// ==/UserScript==
+
+document.onkeyup = function(e) {
+	// when you tyoe E in point cursour to search bar
+	if (e.which == 69) {
+		document.querySelector("input[name='q']").focus();  // duckduckgo.com
+		document.querySelector('input[title]').focus();  // google.com
+	}
+}
